349e6a682d44b13ffb0f39e455374608162aded4
Andrey Sergeev <SergeevAndreyAleksandrovich@yandex.ru>
2026-04-11 00:27:04 +0900

    Created first class

--- LogScanner/src/LogLeverFinder.java
+++ LogScanner/src/LogLeverFinder.java
@@ -1,25 +1,44 @@
 import java.io.File;
 import java.io.IOException;
-import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 import java.io.IOException;
 
 public class LogLeverFinder {
-    public File filePath;
+    String filePath;
 
-    //   Optional<String> file = Optional.ofNullable(null);
+
     public LogLeverFinder(String filePath) {
         this.filePath = filePath;
     }
+    File file = new File(filePath);
+    //Path path = new Path(filePath);
+
     try {
         boolean exists1 = Files.exists(Paths.get(filePath));
         if (exists1 && Files.isReadable(Path.of(filePath))) {
            // URLConnection.guessContentTypeFromName(filePath);
             //Files.probeContentType(Path.of(filePath));
-            String format = filePath.getName().substring(filePath.getName().lastIndexOf(".") + 1);
+            String format = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+            switch (format.toLowerCase()) {
+                case "json":
+                    return findInJson(filePath);
+                    break;
+                case "log":
+                case "txt":
+                    return findInText(filePath);
+                    break;
+                case "xml":
+                    return findInXml(filePath);
+                    break;
+                case "postgresql":
+                    return findInPostgreSQL(filePath);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Неподдерживаемый формат" + format);
+                    break;
+            }
         }
     } catch (IOException e) {
         System.err.printlm("Ошибка в пути файла или прав доступа: " + e.getMessage());
